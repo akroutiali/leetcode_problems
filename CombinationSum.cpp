1e6a9c80c196331d42edea605b4d3e76ab3790bb
akroutiali <84643507+akroutiali@users.noreply.github.com>
2022-01-13 17:17:56 -0500

    Update CombinationSum.cpp

--- CombinationSum.cpp
+++ CombinationSum.cpp
@@ -1,3 +1,23 @@
+/*Algorithmes:
+
+as base case : to check if index equal to size of the input vector we have to check if target equal to 0 to add the add the subsetor not
+
+                   7
+                 
+                 2       
+             5            5
+              2         3    
+          3                  2
+           2               3
+       1   
+        2
+              
+ -check if target <candidates[i] keep doing this operation :target=target-candidates[i]
+ -call  recursive function with the same index 
+ redo step1+step2 until base case is verified 
+ -call the recursive function with index=index+1
+*/
+
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
