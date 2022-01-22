eebd597da4775b206bdbf401d7fd6415cc09107a
akroutiali <84643507+akroutiali@users.noreply.github.com>
2022-01-22 13:23:25 -0500

    Create plaindromeNumberSolution2.cpp

---
+++ plaindromeNumberSolution2.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+
+    /*
+    121
+    121
+    
+    
+    1:121/10=12/10=1
+    */
+    void getdigits(vector<int>&v,int n)
+    {
+        if(n>9)
+        {
+            getdigits(v,n/10);
+        }
+        v.push_back(n%10);
+        
+    }
+    bool isPalindrome(int x) {
+        int n=x;
+        int val=0;
+        int res=0;
+        vector<int>v;
+        getdigits(v,x);
+        int start=0;
+        int end=v.size()-1;
+        while(start<=end)
+        {
+            if(v[start] != v[end])
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+        /*
+        cout<<v[1]<<endl;
+        while(n>0)
+        {
+            res=n%10;
+            n=n/10;
+            val=(val*10)+res;
+            cout<<"val=="<<val<<endl;
+            
+        }
+        if(val==x)
+            return true;
+        else
+            return false;
+        */
+    }
+};
+/*
+323
+
+how to get 3
+3=323%10=3
+x=323%10=32
+2=32%10
+3=3%10
+
+*/
