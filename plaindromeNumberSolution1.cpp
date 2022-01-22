c1d3083780ba469b2fe5c70d3de0e9805a5145f1
akroutiali <84643507+akroutiali@users.noreply.github.com>
2022-01-22 14:36:00 -0500

    Update plaindromeNumberSolution1.cpp

--- plaindromeNumberSolution1.cpp
+++ plaindromeNumberSolution1.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
 
-    /*
-    121
-    121
-    
-    
-    1:121/10=12/10=1
-    */
-    void getdigits(vector<int>&v,int n)
-    {
-        if(n>9)
-        {
-            getdigits(v,n/10);
-        }
-        v.push_back(n%10);
-        
-    }
     bool isPalindrome(int x) {
         int n=x;
         int val=0;
         int res=0;
-        vector<int>v;
-        getdigits(v,x);
-        int start=0;
-        int end=v.size()-1;
-        while(start<=end)
-        {
-            if(v[start] != v[end])
-                return false;
-            start++;
-            end--;
-        }
-        return true;
-        /*
-        cout<<v[1]<<endl;
+
         while(n>0)
         {
             res=n%10;
@@ -47,7 +18,6 @@ public:
             return true;
         else
             return false;
-        */
     }
 };
 /*
