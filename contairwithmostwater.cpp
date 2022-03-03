d062d56080af9e9c09f023f237394e483e96b67f
akroutiali <akroutialy@gmail.com>
2022-03-03 16:54:48 -0500

    11:Container With Most Water

---
+++ contairwithmostwater.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max=INT_MIN;
+        int i=0;
+        int j=height.size()-1;
+        while(i<j)
+        {
+            int ans=(j-i)*min(height[i],height[j]);
+            if(ans>max)
+                max=ans;
+            height[i]>height[j]?j--:i++;
+        }
+        return max;
+        
+    }
+};
+
+time Complexity O(n)
+space complexity O(1)
